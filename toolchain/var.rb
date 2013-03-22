789f957f62f05cc98282fd6d315a90b6ae6eb0d8
Daniel Bittman <danielbittman1@gmail.com>
2013-03-21 23:44:52 -0700

    yeah! It works!

--- toolchain/var.rb
+++ toolchain/var.rb
@@ -9,6 +9,9 @@ $gcc_download = "http://ftp.gnu.org/gnu/gcc/gcc-#{$gcc_ver}/gcc-#{$gcc_ver}.tar.
 $binutils_download = "http://ftp.gnu.org/gnu/binutils/binutils-#{$binutils_ver}.tar.gz"
 $newlib_download = "ftp://sourceware.org/pub/newlib/newlib-#{$newlib_ver}.tar.gz"
 
+# below are default settings for various build system requirements.
+# these work on my system, after installing the appropriate packages
+
 # these are required by newlib
 # path to automake version 1.12
 $automake = "automake-1.12"
@@ -16,6 +19,7 @@ $automake = "automake-1.12"
 $aclocal = "aclocal-1.12"
 
 # and this is required by gcc :(
+# ...version 2.64 of autoconf, that is
 $autoconf = "/opt/autoconf/2.64/bin/autoconf"
 
 # change this... maybe -j3 or so?
