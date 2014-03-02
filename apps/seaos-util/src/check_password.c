1b75bee9094366f95df2c243c54e600e0f45adb2
Daniel Bittman <danielbittman1@gmail.com>
2014-03-01 19:19:38 -0800

    added hashing and salting for passwords

--- apps/seaos-util/src/check_password.c
+++ apps/seaos-util/src/check_password.c
@@ -6,60 +6,168 @@
 #include <string.h>
 #include <signal.h>
 #include <sys/stat.h>
-int check_password(char *username)
+#include <time.h>
+#include <errno.h>
+#include <openssl/sha.h>
+#include <ctype.h>
+
+#define _SHADOW_RIGHT 0
+#define _SHADOW_O_ERR 1
+#define _SHADOW_NOENT 2
+#define _SHADOW_WRONG 3
+#define _SHADOW_F_ERR 4
+
+void get_secure_salt(unsigned char *salt, int length)
 {
-	char fp[strlen(username) + strlen("/users/") + strlen("/.passwd") + 1];
-	sprintf(fp, "/users/%s/.passwd", username);
-	FILE *f = fopen(fp, "r");
-	if(!f) return 1;
-	struct stat st;
-	stat(fp, &st);
-	char ps[st.st_size+1];
-	memset(ps, 0, st.st_size+1);
-	if(fgets(ps, st.st_size+1, f) < 0) {
-		fclose(f);
-		return 1;
+	/* we trust the kernel to give us random numbers */
+	FILE *randfile = fopen("/dev/random", "rb");
+	if(!randfile) {
+		int i;
+		fprintf(stderr, "error opening /dev/random: %s\n", strerror(errno));
+		fprintf(stderr, "warning - using pseudorandom number generator for salting! BAD IDEA.\n");
+		srandom(time(0));
+		for(i=0;i<length;i++) {
+			*(salt+i) = ((double)random()/RAND_MAX) * 0xFF;
+		}
+	} else {
+		fread(salt, sizeof(unsigned char), length, randfile);
+		fclose(randfile);
+	}
+}
+
+void generate_hash(unsigned char *hash, unsigned char *salt, unsigned char *password, int salt_length)
+{
+	SHA512_CTX c;
+	unsigned char buffer[salt_length + strlen(password) + 1];
+	strcpy(buffer, password);
+	memcpy(buffer + strlen(password), salt, salt_length);
+	
+	SHA512_Init(&c);
+	SHA512_Update(&c, buffer, strlen(password) + salt_length);
+	SHA512_Final(hash, &c);
+}
+
+static int __get_raw(int v)
+{
+	if(isdigit(v)) return v - '0';
+	v = tolower(v);
+	return (v - 'a') + 10;
+}
+
+static int verify_hash(char *given_password, char *stored_password)
+{
+	char *stored_salt, *stored_hash, *algorithm;
+	if(*stored_password != '$') /* format error */
+		return _SHADOW_F_ERR;
+	stored_password++; /* skip first $ */
+	
+	algorithm = stored_password;
+	stored_salt = strchr(algorithm, '$');
+	if(!stored_salt)
+		return _SHADOW_F_ERR;
+	*stored_salt=0;
+	stored_salt++; /* skip $ */
+	
+	stored_hash = strchr(stored_salt, '$');
+	if(!stored_hash)
+		return _SHADOW_F_ERR;
+	*stored_hash=0;
+	stored_hash++;
+	
+	unsigned char salt[SHA512_DIGEST_LENGTH];
+	unsigned char hash[SHA512_DIGEST_LENGTH];
+	unsigned char compare_hash[SHA512_DIGEST_LENGTH];
+	int i=0;
+	while(*stored_salt) {
+		unsigned char v1, v2;
+		v1 = *stored_salt;
+		v2 = *(stored_salt+1);
+		v1 = __get_raw(v1);
+		v2 = __get_raw(v2);
+		salt[i] = (v1 << 4) | v2;
+		stored_salt+=2;
+		i++;
+	}
+	i=0;
+	while(*stored_hash) {
+		unsigned char v1, v2;
+		v1 = *stored_hash;
+		v2 = *(stored_hash+1);
+		v1 = __get_raw(v1);
+		v2 = __get_raw(v2);
+		compare_hash[i] = (v1 << 4) | v2;
+		stored_hash+=2;
+		i++;
+	}
+
+	generate_hash(hash, salt, given_password, SHA512_DIGEST_LENGTH);
+	
+	if(!memcmp(hash, compare_hash, SHA512_DIGEST_LENGTH))
+		return _SHADOW_RIGHT;
+	return _SHADOW_WRONG;
+}
+
+int verify_password(char *login, char *given_password)
+{
+	FILE *f = fopen("/etc/shadow", "r");
+	if(!f)
+		return _SHADOW_O_ERR;
+	char buffer[4096];
+	while(fgets(buffer, 4095, f)) {
+		/* search for user's entry */
+		char *col = strchr(buffer, ':');
+		if(!col)
+			continue;
+		/* so we can strcmp */
+		*col = 0;
+		if(!strcmp(buffer, login)) {
+			fclose(f);
+			if(!given_password) return _SHADOW_RIGHT;
+			/* okay, find salt and hash */
+			char *stored_password = col+1;
+			char *delim = strchr(stored_password, ':');
+			if(!delim) /* okay, line ends early. But we can still deal with it... */
+				delim = strchr(stored_password, '\n');
+			if(delim) *delim = 0;
+			return verify_hash(given_password, stored_password);
+		}
 	}
 	fclose(f);
-	printf("%s", "Password: ");
-	fflush(0);
-	char ps_t[128];
-	memset(ps_t, 0, 128);
-	ioctl(1, 1, 0);
-	read(0, ps_t, 128);
-	ioctl(1, 1, 15);
-	char *w = strchr(ps_t, '\n');
-	if(w) *w=0;
-	if(!strcmp(ps_t, ps))
-		return 1;
-	return 0;
+	return _SHADOW_NOENT;
 }
 
 int check_password2(char *prompt, char *username)
 {
-	char fp[strlen(username) + strlen("/users/") + strlen("/.passwd") + 1];
-	sprintf(fp, "/users/%s/.passwd", username);
-	FILE *f = fopen(fp, "r");
-	if(!f) return 1;
-	struct stat st;
-	stat(fp, &st);
-	char ps[st.st_size+1];
-	memset(ps, 0, st.st_size+1);
-	if(fgets(ps, st.st_size+1, f) < 0) {
-		fclose(f);
+	char buffer[128];
+	
+	if(verify_password(username, 0) != _SHADOW_RIGHT)
 		return 1;
-	}
-	fclose(f);
+	
+	memset(buffer, 0, 128);
 	printf("%s", prompt);
 	fflush(0);
-	char ps_t[128];
-	memset(ps_t, 0, 128);
-	ioctl(1, 1, 0);
-	read(0, ps_t, 128);
-	ioctl(1, 1, 15);
-	char *w = strchr(ps_t, '\n');
-	if(w) *w=0;
-	if(!strcmp(ps_t, ps))
-		return 1;
-	return 0;
+	
+	if(!fgets(buffer, 128, stdin)) return 0;
+	
+	char *nl = strchr(buffer, '\n');
+	if(nl) *nl=0;
+	
+	int r = verify_password(username, buffer);
+	switch(r) {
+		case _SHADOW_F_ERR:
+			fprintf(stderr, "shadow file format error!\n");
+			/* fall through */
+		case _SHADOW_O_ERR:
+		case _SHADOW_NOENT:
+		case _SHADOW_RIGHT:
+			return 1;
+		case _SHADOW_WRONG:
+			return 0;
+	}
+	return 1;
+}
+
+int check_password(char *username)
+{
+	return check_password2("Password: ", username);
 }
--- apps/seaos-util/src/passwd.c
+++ apps/seaos-util/src/passwd.c
@@ -5,13 +5,24 @@
 #include <errno.h>
 #include <libgen.h>
 #include <unistd.h>
+#include <openssl/sha.h>
+#include <time.h>
 
 char *prog = 0;
+
+void get_secure_salt(unsigned char *salt, int length);
+void generate_hash(unsigned char *hash, unsigned char *salt, unsigned char *password, int salt_length);
+int verify_password(char *login, char *given_password);
 
 void usage(int e)
 {
 	fprintf(stderr, "%s: usage: [-fh] login\n  -f  don't ask for old password if root\n  -h  display this message\n\n", prog);
 	exit(e);
+}
+
+void err_exit(char *msg) {
+	fprintf(stderr, "%s: %s: %s\n", prog, msg, strerror(errno));
+	exit(1);
 }
 
 int main(int argc, char **argv)
@@ -97,25 +108,82 @@ int main(int argc, char **argv)
 		return 4;
 	}
 	
-	char fp[strlen(login) + strlen("/users/") + strlen("/.passwd") + 1];
-	sprintf(fp, "/users/%s/.passwd", login);
-	if(!new[0]) {
-		if(unlink(fp) && errno != ENOENT) {
-			fprintf(stderr, "%s: /users/%s/.passwd: %s\n", prog, login, strerror(errno));
-			return 5;
-		}
-	} else {
-		FILE *f = fopen(fp, "w");
-		if(!f) {
-			fprintf(stderr, "%s: /users/%s/.passwd: %s\n", prog, login, strerror(errno));
-			return 5;
-		}
-		fprintf(f, "%s", new);
-		fclose(f);
-		if(chown(fp, p->pw_uid, p->pw_gid))
-			fprintf(stderr, "%s: could not change owner of /users/%s/.passwd: %s\n", prog, login, strerror(errno));
-		if(chmod(fp, 0600))
-			fprintf(stderr, "%s: could not change permissions of /users/%s/.passwd: %s\n", prog, login, strerror(errno));
+	unsigned char salt[SHA512_DIGEST_LENGTH];
+	unsigned char hash[SHA512_DIGEST_LENGTH];
+	get_secure_salt(salt, SHA512_DIGEST_LENGTH);
+	
+	generate_hash(hash, salt, new, SHA512_DIGEST_LENGTH);
+	
+	FILE *shadow_old = fopen("/etc/shadow", "r");
+	if(!shadow_old && errno != ENOENT) {
+		fprintf(stderr, "%s: cannot open /etc/shadow, but the file may exist! (%s)\n", strerror(errno));
+		exit(1);
 	}
+	FILE *shadow_new = fopen("/etc/shadow.new", "w");
+	if(!shadow_new) {
+		fprintf(stderr, "%s: could not open /etc/shadow.new: %s\n", prog, strerror(errno));
+		exit(1);
+	}
+	
+	if(shadow_old) {
+		unsigned char buffer[4096];
+		memset(buffer, 0, 4096);
+		while(fgets(buffer, 4095, shadow_old)) {
+			char *tmp = strchr(buffer, ':');
+			if(tmp) {
+				*tmp = 0;
+				if(strcmp(login, buffer)) {
+					*tmp = ':';
+					fputs(buffer, shadow_new);
+				}
+			}
+		}
+
+		fclose(shadow_old);
+	}
+	
+	if(fprintf(shadow_new, "%s:$6$", login) < 0){
+		err_exit("error writing /etc/shadow_new");
+	}
+	
+	int j;
+	for(j=0;j<SHA512_DIGEST_LENGTH;j++) {
+		if(fprintf(shadow_new, "%02x", *(salt+j)) < 0){
+			err_exit("error writing /etc/shadow_new");
+		}
+	}
+	if(fputc('$', shadow_new) == EOF){
+		err_exit("error writing /etc/shadow_new");
+	}
+	for(j=0;j<SHA512_DIGEST_LENGTH;j++) {
+		if(fprintf(shadow_new, "%02x", *(hash+j)) < 0){
+			err_exit("error writing /etc/shadow_new");
+		}
+	}
+	
+	if(fputc('\n', shadow_new) == EOF){
+		err_exit("error writing /etc/shadow_new");
+	}
+	
+	fclose(shadow_new);
+	
+	if(shadow_old) {
+		if(rename("/etc/shadow", "/etc/shadow.old") == -1) {
+			err_exit("failed to rename /etc/shadow");
+		}
+	}
+	
+	if(rename("/etc/shadow.new", "/etc/shadow") == -1) {
+		if(rename("/etc/shadow.old", "/etc/shadow") == -1) {
+			err_exit("!! failed to restore /etc/shadow.old to /etc/shadow !!");
+		}
+		err_exit("failed to rename /etc/shadow.new");
+	}
+	if(shadow_old) {
+		if(unlink("/etc/shadow.old") == -1) {
+			err_exit("failed to unlink /etc/shadow.old (but /etc/shadow was updated)");
+		}
+	}
+	
 	return 0;
 }
