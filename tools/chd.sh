92dc94b7e59d63f59ff5436c359303759187f711
Daniel Bittman <danielbittman1@gmail.com>
2014-03-04 15:59:58 -0800

    FS permissions are now correct

--- tools/chd.sh
+++ tools/chd.sh
@@ -5,24 +5,27 @@ rm -f hd.img 2>/dev/null
 echo -n "processing hd.img..."
 dd if=/dev/zero of=hd.img bs=1024 count=1 2> /dev/null
 dd if=/dev/zero of=hd.img bs=1024 count=1 seek=1999999 2> /dev/null
+echo -n "partition..."
 
-sfdisk -q -L -u -S63 -H16 hd.img 2>/dev/null << EOF
+sfdisk -q -L -u -S63 -H16 hd.img &>/dev/null << EOF
 2048,,,*
 EOF
 
+echo -n "formatting..."
 losetup -o1048576 $loop hd.img
 mke2fs -q -I128 -b1024 $loop 
 sync
 losetup -d $loop
 
+echo "installing grub..."
 sh ./tools/open_hdimage.sh
 mkdir -p ./mnt/boot/grub
 cp -r data/boot/grub/* ./mnt/boot/grub/
 sh ./tools/close_hdimage.sh
 
-(echo -e "device (hd0) hd.img\nroot (hd0,0)\nembed /boot/grub/e2fs_stage1_5 (hd0)\ninstall (hd0,0)/boot/grub/stage1 (hd0) (hd0)1+17 p (hd0,0)/boot/grub/stage2\nquit" | grub --device-map data/boot/grub/device.map --batch)
+(echo -e "device (hd0) hd.img\nroot (hd0,0)\nembed /boot/grub/e2fs_stage1_5 (hd0)\ninstall (hd0,0)/boot/grub/stage1 (hd0) (hd0)1+17 p (hd0,0)/boot/grub/stage2\nquit" | grub --device-map data/boot/grub/device.map --batch > /dev/null)
 
-echo processing hd2.img..
+echo processing hd2.img...
 dd if=/dev/zero of=hd2.img bs=1024 count=1 2> /dev/null
 dd if=/dev/zero of=hd2.img bs=1024 count=1 seek=100000 2> /dev/null
 yes | (/sbin/mke2fs -q -I128 -b1024 hd2.img) > /dev/null
--- tools/copy_to_hd.sh
+++ tools/copy_to_hd.sh
@@ -2,7 +2,7 @@
 
 sh ./tools/open_hdimage.sh
 echo copying ported programs to hd.img...
-cp -f -r apps/data-$1/* ./mnt
+cp -f -r apps/data-$1/* ./mnt &>/dev/null
 cp -f -r apps/install-base-$1/* ./mnt
 rm -rf `find ./mnt -name man 2>/dev/null`
 echo copying data/ to hd.img...
@@ -13,9 +13,18 @@ echo copying source code to hd.img...
 mkdir -p ./mnt/usr/src
 cp -rf seakernel ./mnt/usr/src/sea
 cp -rf apps/seaos-util ./mnt/usr/src/seaos-util
-make -C ./mnt/usr/src/sea clean
+echo cleaning up hd.img...
+make -s -C ./mnt/usr/src/sea clean
 rm ./mnt/usr/src/sea/tools/confed ./mnt/usr/src/sea/tools/mkird
 rm -rf `find ./mnt -name .git 2>/dev/null`
 rm -f `find ./mnt -name .directory 2>/dev/null`
+
+echo fixing permissions...
+chown -R root ./mnt/*
+chgrp -R 0 ./mnt/*
+chmod u+s ./mnt/bin/login
+chmod u+s ./mnt/bin/passwd
+chmod o-r ./mnt/etc/shadow
+chmod g-r ./mnt/etc/shadow
 
 sh ./tools/close_hdimage.sh
