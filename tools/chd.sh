871b98ea998d872faff2f93d1c67bd6244f6194d
Daniel Bittman <danielbittman1@gmail.com>
2015-08-31 18:39:21 -0700

    cleaned up chd.sh

--- tools/chd.sh
+++ tools/chd.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 loop=`cat .loop`
-
+echo generating $2
 rm -f $2 2>/dev/null
-echo -n "processing $2..."
 dd if=/dev/zero of=$2 bs=1024 count=1 2> /dev/null
 dd if=/dev/zero of=$2 bs=1024 count=1 seek=1999999 2> /dev/null
-echo -n "partition..."
 
-sfdisk -q $2 << EOF
+sfdisk -q $2 &>/dev/null << EOF
 2048,,,*
 EOF
 sfdisk --activate $2 1
-echo -n "formatting..."
+echo "formatting..."
 sudo losetup /dev/loop1 $2
 sudo losetup -o1048576 /dev/loop2 $2
 mke2fs -q -I128 -b1024 /dev/loop2 
@@ -19,7 +17,7 @@ sync
 mount /dev/loop2 /mnt
 
 echo "installing grub..."
-sudo grub-install --root-directory=/mnt --no-floppy --modules="normal part_msdos ext2 multiboot" /dev/loop1
+sudo grub-install --root-directory=/mnt --locales= --no-floppy --modules="normal part_msdos ext2 multiboot" /dev/loop1
 umount /mnt
 losetup -d /dev/loop2
 losetup -d /dev/loop1
